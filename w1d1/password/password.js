3d91b9ff12c6d00713931a8be31f033066f9c1a4
Tyler <tyler.tomczyk@gmail.com>
2019-02-06 16:25:41 +0000

    commit final answer

--- w1d1/password/password.js
+++ w1d1/password/password.js
@@ -8,23 +8,130 @@ Every "l" (el) in the string should be replaced with a "1" (one).
 
 */
 
+//function to obfuscate string, not to read from argv directly
+function obf(char){
+ if(char.toLowerCase() === "a") {
+   return 4;
+ }
+ else if(char.toLowerCase() === "e") {
+   return 3;
+ }
+ else if(char.toLowerCase() === "o") {
+   return 0;
+ }
+ else if(char.toLowerCase() === "l") {
+   return 1;
+ }
+ else{
+   return char;
+ }
+}
+
+//read from command line and pass to obf function
+function read(){
+ var args = process.argv;
+ var final = '';
+
+ for(var i = 0; i < args[2].length; i++){
+   final += obf(args[2].charAt(i));
+ }
+
+ return final;
+}
+
+console.log(read());
+
+
+
+
+
+/*
+
+
 function obf (pass) {
   output = ''
   var n = pass.length
     for (var i = 0; i < n; i++) { //this seems to have the correct loop
-      if (pass.charAt(i) !== 'a' || pass.charAt(i) !== 'e' || pass.charAt(i) !== 'o' || pass.charAt(i) !== 'l') {
-        output = output + pass.charAt(i)
+      if (pass.charAt(i) === 'a') {
+        console.log(pass.charAt(i))
+        output += 4
+        console.log ('-------', output)
+      }
+      else if (pass.charAt(i) === 'e') {
+        output += 3
+      }
+      else if (pass.charAt(i) === 'o') {
+        return 0
+      }
+      else if (pass.charAt(i) === 'l') {
+        return 1
       }
       else {
-    output = output + 'A'
+      pass.charAt(i)
     }
     return output + pass.length
     }
 }
 
+
+//read from argv and pass to obf
+function read(){
+ var args = process.argv;
+ var final = “”;
+
+ for(var i = 0; i < args[2].length; i++){
+   final += obf(args[2].charAt(i));
+ }
+
+
+ return final;
+}
+
+console.log(read());
+
+*/
+
 //console.log(pass.length)
 
-console.log(obf('password'))
+//console.log(obf('password'))
 
 
 // try this method maybe ? https://www.w3schools.com/js/js_switch.asp
+
+
+
+
+/*
+
+function obf (pass) {
+
+
+  if (pass() === 'a') {
+    console.log('-----',pass)
+    return 4;
+  }
+  else if (pass() === 'e') {
+    return 3;
+  }
+  else if (pass() === 'o') {
+    return 0;
+  }
+  else if (pass() === 'l') {
+    return 1;
+  }
+  else {
+    return pass;
+  }
+}
+
+
+
+
+
+console.log(obf('password'))
+
+*/
+
+
+
+
